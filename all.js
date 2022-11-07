6e298ede5bb5fdfe5addc0be5fcfdd2a570702f1
kelvinLin9 <kelvin80121@hotmail.com>
2022-11-07 11:51:44 +0800

    update

--- all.js
+++ all.js
@@ -1,29 +1,84 @@
 // 切換
-const signBtn = document.querySelector('.sing-btn')
-const loginBtn = document.querySelector('.login-btn')
-const signUp = document.querySelector('.signup')
-const login = document.querySelector('.login')
-// console.log(signUp.innerHTML)
+const toSignUpPage = document.querySelector('.to-signup-page')
+const toLoginPage = document.querySelector('.to-login-page')
+const signUpPage = document.querySelector('.signup-page')
+const loginPage = document.querySelector('.login-page')
+// console.log(signUpPage.innerHTML)
 
-signBtn.addEventListener('click', () => {
-  login.classList.toggle('active')
-  signUp.classList.toggle('active')
+toSignUpPage.addEventListener('click', () => {
+  loginPage.classList.toggle('active')
+  signUpPage.classList.toggle('active')
 })
-loginBtn.addEventListener('click', () => {
-  login.classList.toggle('active')
-  signUp.classList.toggle('active')
+toLoginPage.addEventListener('click', () => {
+  loginPage.classList.toggle('active')
+  signUpPage.classList.toggle('active')
 })
 
 // 註冊
+const apiUrl = 'https://todoo.5xcamp.us'
 const signUpEmail = document.querySelector('#signupEmail')
 const nickName = document.querySelector('#nickName')
 const signUpPassword = document.querySelector('#signUpPassword')
 const signUpPassword2 = document.querySelector('#signUpPassword2')
-const btn2 = document.querySelector('.btn2')
-btn2.addEventListener('click', () => {
+const signupBtn = document.querySelector('.signup-btn')
+signupBtn.addEventListener('click', () => {
   let userEmail = signUpEmail.value
   let userNickName = nickName.value
   let userSignUpPassword = signUpPassword.value
   let userSignUpPassword2 = signUpPassword2.value
-  console.log(userEmail, userNickName, userSignUpPassword, userSignUpPassword2)
+  signUp(userEmail, userNickName, userSignUpPassword)
+})
+function signUp(userEmail, userNickName, userSignUpPassword) {
+  console.log(userEmail, userNickName, userSignUpPassword)
+  axios.post(`${apiUrl}/users`,{
+    "user": {
+      "email": userEmail,
+      "nickname": userNickName,
+      "password": userSignUpPassword
+    }
+  })
+  .then(res => {
+    alert(res.data.message)
+  })
+  .catch(res => {
+    alert(res.message)
+  })
+}
+
+// 登入
+const loginEmail = document.querySelector('#loginEmail')
+const loginPassword = document.querySelector('#loginPassword')
+const loginBtn = document.querySelector('.login-btn')
+loginBtn.addEventListener('click', () => {
+  let userEmail = loginEmail.value
+  let userPassword = loginPassword.value
+  login(userEmail, userPassword)
+})
+function login(userEmail, userPassword){
+  axios.post(`${apiUrl}/users/sign_in`,{
+    "user": {
+      "email": userEmail,
+      "password": userPassword
+    }
+  })
+  .then(res => {
+    alert(res.data.message)
+    toListPage()
+  })
+  .catch(res => {
+    alert(res.message)
+  })
+}
+
+// 切換到待辦頁面&登出
+const userPage = document.querySelector('.user-page')
+const todoPage = document.querySelector('.todo-page')
+const signOut = document.querySelector('.sign-out')
+function toListPage(){
+  userPage.classList.toggle('active')
+  todoPage.classList.toggle('active')
+}
+signOut.addEventListener('click',() => {
+  userPage.classList.toggle('active')
+  todoPage.classList.toggle('active')
 })
